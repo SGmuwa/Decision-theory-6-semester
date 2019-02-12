ec7d7293bcd7e1803d02b9a34cc1ac5fd18742a7
SG_Muwa <motherlode.muwa@gmail.com>
2019-02-12 23:06:09 +0300

    fix link 2005

--- DecisionTheoryPrograms/program.c
+++ DecisionTheoryPrograms/program.c
@@ -1,6 +1,9 @@
-#include "simplex_test.h"
+#include <stdio.h>
+extern int Simplex_test_main();
 
 int main(int argc, char argv[]) {
-	return Simplex_test_main();
+	int err = Simplex_test_main();
+	scanf_s("%*s");
+	return err;
 }
 
