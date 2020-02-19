e126b7579afbb43678ffcfbfd398c17b724425f6
SG_Muwa <motherlode.muwa@gmail.com>
2020-02-20 02:33:51 +0300

    Добавлен хороший функционал печати таблиц.

--- Simplex/Program.cs
+++ Simplex/Program.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
+using System.Text;
 
-public class Simplex
+public static class Simplex
 {
     public static void Main(string[] args)
     {
@@ -37,14 +39,7 @@ public class Simplex
                     if (i == tempN + 1) tableSimplex[i, j] = x[j];
             tableSimplex[tempN + 1, n] = TargetFunction(x);
         }
-        for (int i = 0; i < n + 1; i++)
-        {
-            for (int t = 0; t < n + 1; t++)
-            {
-                Console.Write(tableSimplex[i, t] + "\t");
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine(tableSimplex.TableToString());
         while (true)
         {
             Console.WriteLine("Итерация " + iteration++);
@@ -57,7 +52,7 @@ public class Simplex
                 for (int t = 0; t < n + 1; t++)
                     if (t == n)
                         arrayFuncValue[i] = tableSimplex[i, t];
-            int maxVertex = SearchMax(arrayFuncValue);
+            int maxVertex = Array.IndexOf(arrayFuncValue, arrayFuncValue.Max());
             for (int i = 0; i < n + 1; i++)
                 for (int t = 0; t < n; t++)
                     if (i == maxVertex)
@@ -90,7 +85,7 @@ public class Simplex
                     for (int t = 0; t < n + 1; t++)
                         if (t == n)
                             arrayFuncValue[i] = tableSimplex[i, t];
-                int minVertex = SearchMin(arrayFuncValue);
+                int minVertex = Array.IndexOf(arrayFuncValue, arrayFuncValue.Min());
                 for (int i = 0; i < n + 1; i++)
                     for (int t = 0; t < n + 1; t++)
                     {
@@ -103,7 +98,7 @@ public class Simplex
                         }
                         else
                             if (t == n & i != minVertex)
-                                tableSimplex[i, t] = TargetFunction(start);
+                            tableSimplex[i, t] = TargetFunction(start);
                         for (int k = 0; k < start.Length; k++)
                             start[k] = 0;
                     }
@@ -111,17 +106,12 @@ public class Simplex
             for (int i = 0; i < centerOfGravityXc.Length; i++)
                 centerOfGravityXc[i] = 0;
             for (int i = 0; i < n + 1; i++)
-            {
                 for (int t = 0; t < n + 1; t++)
-                {
                     if (t < n)
                         centerOfGravityXc[t] =
                             centerOfGravityXc[t] +
                             1.0 / (n + 1) * tableSimplex[i, t];
-                    Console.Write(tableSimplex[i, t] + "\t");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine(tableSimplex.TableToString());
             double centerY = TargetFunction(centerOfGravityXc);
             Console.WriteLine($"Центр тяжести симплекса: f({string.Join(", ", centerOfGravityXc)}) = {centerY}");
 
@@ -138,7 +128,7 @@ public class Simplex
                     for (int t = 0; t < n + 1; t++)
                         if (t == n)
                             arrayFuncValue[i] = tableSimplex[i, t];
-                int minVertex = SearchMin(arrayFuncValue);
+                int minVertex = Array.IndexOf(arrayFuncValue, arrayFuncValue.Min());
                 double resultMin = tableSimplex[minVertex, n];
                 Console.WriteLine("Минимум: " + resultMin);
                 return;
@@ -152,29 +142,52 @@ public class Simplex
         4.2 * mas[1] +
         2.8 * Math.Pow(mas[1], 2);
 
-    public static int SearchMax(double[] mas)
-    {
-        double max = mas[0];
-        int ind = 0;
-        for (int i = 1; i < mas.Length; i++)
-            if (max < mas[i])
-            {
-                max = mas[i];
-                ind = i;
-            }
-        return ind;
-    }
+    /// <summary>
+    /// Преобразует объект в строку заданного формата, вставляя дополнительные пробелы,
+    /// чтобы подогнать под размер <code>len</code>.
+    /// </summary>
+    /// <param name="toInsert">Объект, который надо преобразовать в строку.</param>
+    /// <param name="len">Минимальная длинна выходной строки.</param>
+    /// <param name="format">Формат вывода.</param>
+    /// <returns>Строка, представляющая объект <code>toInstert</code>.</returns>
+    internal static string ToString(this object toInsert, int len, string format)
+                => string.Format(string.Format("{{0, {0} :{1}}}", len, format), toInsert);
 
-    public static int SearchMin(double[] mas)
+    /// <summary>
+    /// Превращает двухмерную таблицу в строку.
+    /// </summary>
+    /// <param name="input">Таблица.</param>
+    /// <param name="format">Формат вывода каждого элемента.</param>
+    /// <param name="renderForeach">Функция преобразования </param>
+    /// <returns>Строковое представление каждого элемента массива в виде таблицы.</returns>
+    internal static string TableToString(this Array input, string format = null, Func<dynamic, object> renderForeach = null)
     {
-        double min = mas[0];
-        int ind = 0;
-        for (int i = 1; i < mas.Length; i++)
-            if (min > mas[i])
+        if (input.Rank != 2)
+            throw new NotSupportedException();
+        Array array;
+        if (renderForeach == null)
+            array = input;
+        else
+        {
+            array = new object[input.GetLength(0), input.GetLength(1)];
+            for (int y = 0; y < array.GetLength(0); y++)
+                for (int x = 0; x < array.GetLength(1); x++)
+                    array.SetValue(renderForeach(input.GetValue(y, x)), y, x);
+        }
+        int max = -1;
+        foreach (var a in array)
+            if (a.ToString(0, format).Length > max)
+                max = a.ToString(0, format).Length;
+        max++;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
             {
-                min = mas[i];
-                ind = i;
+                sb.Append(array.GetValue(i, j).ToString(max, format));
             }
-        return ind;
+            sb.Append('\n');
+        }
+        return sb.ToString();
     }
 }
