f25ead46bc6acf5ca8dde287cbcae7ba5aca1742
SG_Muwa <motherlode.muwa@gmail.com>
2020-02-21 11:23:13 +0300

    Изменён стиль комментариев

--- PatternSearch/Program.cs
+++ PatternSearch/Program.cs
@@ -4,21 +4,21 @@ public class PatternSearch
 {
     public static void Main(string[] args)
     {
-        int n = 2; //Размерность
-        int d = 2; //Коэффициент уменьшения шага
-        double E = 0.1; //Точность поиска
-        double m = 0.5; //Ускоряющий множитель
-        double h = 0.2; //Шаг
+        int n = 2; // Размерность
+        int d = 2; // Коэффициент уменьшения шага
+        double E = 0.1; // Точность поиска
+        double m = 0.5; // Ускоряющий множитель
+        double h = 0.2; // Шаг
 
-        double[] base_point = new double[n]; //Координаты базисной точки
-        double[] xP = new double[n]; //Координаты точки для поиска по образцу
-        double[] current_point = new double[n]; //x1
-        double[] test_point = new double[n]; //координаты тестовых точек временно храняться в этом массиве
+        double[] base_point = new double[n]; // Координаты базисной точки
+        double[] xP = new double[n]; // Координаты точки для поиска по образцу
+        double[] current_point = new double[n];
+        double[] test_point = new double[n]; // Координаты тестовых точек временно хранятся в этом массиве
 
         for (int i = 0; i < n; i++)
             base_point[i] = 0;
 
-        //считаем тестовую, если она больше текущей, то производится поиск по следующим направлениям и координатам
+        // Считаем тестовую, если она больше текущей, то производится поиск по следующим направлениям и координатам
         bool k = true;
         while (k)
         {
@@ -27,11 +27,11 @@ public class PatternSearch
                 if (i == 0)
                     for (int j = 0; j < n; j++)
                     {
-                        test_point[j] = base_point[j]; //x1
-                        current_point[j] = base_point[j]; //x1
+                        test_point[j] = base_point[j];
+                        current_point[j] = base_point[j];
                     }
                 for (int l = 0; l < n; l++)
-                    test_point[l] = current_point[l]; //x1
+                    test_point[l] = current_point[l];
                 double temp = test_point[i];
                 test_point[i] = temp + h;
                 if (TargetFunction(test_point) < TargetFunction(current_point))
@@ -54,20 +54,20 @@ public class PatternSearch
                     }
                 }
             }
-            //сравнение с базисной точкой x0
+            // Сравнение с базисной точкой x0
             int flag = 0;
             for (int i = 0; i < n; i++)
                 if (base_point[i] == current_point[i])
                     flag += 1;
             if (flag == n)
-            { //если х1 = х0, то уменьшаем шаг
+            { // Если х1 = х0, то уменьшаем шаг
                 double tempH = h;
                 h = (double)tempH / d;
                 Console.WriteLine("НЕ нашли точку (x0=x1)");
                 Console.WriteLine("Уменьшение шага, H = " + h + "\n\n");
             }
             else
-            { //если х1!=х0, то поиск по образцу
+            { // Если х1!=х0, то поиск по образцу
                 Console.Write("Новая базисная точка x1: " + current_point[0] + "   " + current_point[1] + "\n");
                 for (int j = 0; j < n; j++)
                     xP[j] = current_point[j] + (double)m * (current_point[j] - base_point[j]);
