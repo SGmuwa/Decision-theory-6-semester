e95feab75c15ab1f4e12f146703c943ea0c64035
SG_Muwa <motherlode.muwa@gmail.com>
2020-02-28 22:18:06 +0300

    GradientDescentConstStep: Реализован алгоритм печати ровных таблиц для стека

--- GradientDescentConstStep/Program.cs
+++ GradientDescentConstStep/Program.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
-public class GradientDescentConstStep
+public static class GradientDescentConstStep
 {
     public static void Main(string[] args)
     {
@@ -59,10 +61,15 @@ public class GradientDescentConstStep
             Console.WriteLine("x1 = " + el.x1 + "\t x2 = " + el.x2 + "\t f = " + el.result);
         }
     }
-    public static double objectiveFunction(double[] mas)
-    {
-        return ((double)13 / 5) * (Math.Pow(mas[0], 2)) - ((double)21 / 10) * mas[1] + ((double)7 / 5) * (Math.Pow(mas[1], 2));
-    }
+    public static double TargetFunction(double x, double y)
+        => -3.3 * x +
+        5.2 * Math.Pow(x, 2) -
+        4.2 * y +
+        2.8 * Math.Pow(y, 2);
+
+    public static double TargetFunction(double[] args)
+        => TargetFunction(args[0], args[1]);
+
     public static double[] gradientSearch(double[] mas)
     {
         double[] masResult = new double[mas.Length];
@@ -80,17 +87,130 @@ public class GradientDescentConstStep
         }
         return masResult;
     }
+
+    internal static string TableToString<T>(this Stack<IReadOnlyCollection<T>> input, string format = null, Func<dynamic, object> renderForeach = null)
+    {
+        T[,] two = new T[input.Count, input.Peek().Count];
+        {
+            int i = 0;
+            foreach(IEnumerable<T> line in input)
+            {
+                int j = 0;
+                foreach(T element in line)
+                {
+                    two[i, j] = element;
+                    j++;
+                }
+                i++;
+            }
+        }
+        return two.TableToString(format, renderForeach);
+    }
+
+    /// <summary>
+    /// Преобразует объект в строку заданного формата, вставляя дополнительные пробелы,
+    /// чтобы подогнать под размер <code>len</code>.
+    /// </summary>
+    /// <param name="toInsert">Объект, который надо преобразовать в строку.</param>
+    /// <param name="len">Минимальная длинна выходной строки.</param>
+    /// <param name="format">Формат вывода.</param>
+    /// <returns>Строка, представляющая объект <code>toInstert</code>.</returns>
+    internal static string ToString(this object toInsert, int len, string format)
+                => string.Format(string.Format("{{0, {0} :{1}}}", len, format), toInsert);
+
+    internal static IEnumerable<O> EveryConverter<T, O>(this IEnumerable<T> that, Func<T, O> converter)
+    {
+        foreach (var e in that)
+            yield return converter(e);
+    }
+
+    /// <summary>
+    /// Превращает двухмерную таблицу в строку.
+    /// </summary>
+    /// <param name="input">Таблица.</param>
+    /// <param name="format">Формат вывода каждого элемента.</param>
+    /// <param name="renderForeach">Функция преобразования </param>
+    /// <returns>Строковое представление каждого элемента массива в виде таблицы.</returns>
+    internal static string TableToString(this Array input, string format = null, Func<dynamic, object> renderForeach = null)
+    {
+        if (input.Rank != 2)
+            throw new NotSupportedException();
+        Array array;
+        if (renderForeach == null)
+            array = input;
+        else
+        {
+            array = new object[input.GetLength(0), input.GetLength(1)];
+            for (int y = 0; y < array.GetLength(0); y++)
+                for (int x = 0; x < array.GetLength(1); x++)
+                    array.SetValue(renderForeach(input.GetValue(y, x)), y, x);
+        }
+        int max = -1;
+        foreach (var a in array)
+            if (a.ToString(0, format).Length > max)
+                max = a.ToString(0, format).Length;
+        max++;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                sb.Append(array.GetValue(i, j).ToString(max, format));
+            sb.Append('\n');
+        }
+        return sb.ToString();
+    }
 }
 
-public class Element
+public class Element : IEnumerable<double>, ICollection<double>, IReadOnlyCollection<double>
 {
-    public double x1;
-    public double x2;
-    public double result;
+    private readonly double[] memory = new double[3];
+    public double x1 { set => memory[0] = value; get => memory[0]; }
+    public double x2 { set => memory[1] = value; get => memory[1]; }
+    public double result { set => memory[2] = value; get => memory[2]; }
+
+    public int Count => ((ICollection<double>)memory).Count;
+
+    public bool IsReadOnly => ((ICollection<double>)memory).IsReadOnly;
+
     public Element(double x1, double x2, double result)
     {
         this.x1 = x1;
         this.x2 = x2;
         this.result = result;
+    }
+
+    public IEnumerator<double> GetEnumerator()
+    {
+        return ((IEnumerable<double>)memory).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable<double>)memory).GetEnumerator();
+    }
+
+    public void Add(double item)
+    {
+        ((ICollection<double>)memory).Add(item);
+    }
+
+    public void Clear()
+    {
+        ((ICollection<double>)memory).Clear();
+    }
+
+    public bool Contains(double item)
+    {
+        return ((ICollection<double>)memory).Contains(item);
+    }
+
+    public void CopyTo(double[] array, int arrayIndex)
+    {
+        ((ICollection<double>)memory).CopyTo(array, arrayIndex);
+    }
+
+    public bool Remove(double item)
+    {
+        return ((ICollection<double>)memory).Remove(item);
     }
 }
