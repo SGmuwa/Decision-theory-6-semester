475657081863a64c4eb212f7c5cff93619722491
SG_Muwa <motherlode.muwa@gmail.com>
2020-02-28 21:21:06 +0300

    GradientDescentConstStep: Изменён код программирования на C#

--- GradientDescentConstStep/Program.cs
+++ GradientDescentConstStep/Program.cs
@@ -1,7 +1,8 @@
-package tpr_6sem_4laba;
-import java.util.*;
-public class TPR_6sem_4Laba {
-    public static void main(String[] args) {
+using System;
+using System.Collections.Generic;
+
+public class GradientDescentConstStep {
+    public static void Main(string[] args) {
         double E = 0.1;
         double h = 0.4;
         double gradientNorm;
@@ -13,7 +14,7 @@ public class TPR_6sem_4Laba {
         Stack<Element> stack = new Stack<Element>();
         double func = objectiveFunction(mas);
         Element elem = new Element(mas[0],mas[1],func);
-        stack.add(elem);
+        stack.Push(elem);
         gradientMas = gradientSearch(mas);
         double funcResMas;
         do{
@@ -23,48 +24,48 @@ public class TPR_6sem_4Laba {
                     mas[i] = doubleTempMas - h*gradientMas[i];
                 }
                 funcResMas = objectiveFunction(mas);
-                if(stack.peek().result<funcResMas){//уменьшение шага 
+                if(stack.Peek().result<funcResMas){//уменьшение шага 
                     double tempH = h;
                     h=tempH/2;
-                    mas[0] = stack.peek().x1;
-                    mas[1] = stack.peek().x2;
+                    mas[0] = stack.Peek().x1;
+                    mas[1] = stack.Peek().x2;
                 }
-            }while(stack.peek().result<funcResMas);
+            }while(stack.Peek().result<funcResMas);
             
             elem = new Element(mas[0],mas[1],funcResMas);
-            stack.add(elem);
+            stack.Push(elem);
         
             gradientMas = gradientSearch(mas);
             gradientNorm =0;
             for(int i=0;i<n; i++)
-                gradientNorm+=Math.pow(gradientMas[i],2);
+                gradientNorm+=Math.Pow(gradientMas[i],2);
             double grTemp=gradientNorm;
-            gradientNorm = Math.sqrt(grTemp);
+            gradientNorm = Math.Sqrt(grTemp);
         }while(gradientNorm>E);
         
        
-        System.out.println("ОТВЕТ: x1 = "+stack.peek().x1+"\t x2 = "+stack.peek().x2+"\t f = "+stack.peek().result);
-        System.out.println("\nИстория рассматриваемых точек (от минимальной до первой рассмариваемой)");
-        int sizeStack = stack.size();
+        Console.WriteLine("ОТВЕТ: x1 = "+stack.Peek().x1+"\t x2 = "+stack.Peek().x2+"\t f = "+stack.Peek().result);
+        Console.WriteLine("\nИстория рассматриваемых точек (от минимальной до первой рассмариваемой)");
+        int sizeStack = stack.Count;
         for(int i=0;i<sizeStack ;i++){
-            Element el= stack.pop();
-            System.out.println("x1 = "+el.x1+"\t x2 = "+el.x2+"\t f = "+el.result);
+            Element el= stack.Pop();
+            Console.WriteLine("x1 = "+el.x1+"\t x2 = "+el.x2+"\t f = "+el.result);
         }
     }
-    public static double objectiveFunction (double mas[]){
-        return ((double) 13/5)*(Math.pow(mas[0],2)) - ((double) 21/10)*mas[1] + ((double) 7/5)*(Math.pow(mas[1],2)) ;
+    public static double objectiveFunction (double[] mas){
+        return ((double) 13/5)*(Math.Pow(mas[0],2)) - ((double) 21/10)*mas[1] + ((double) 7/5)*(Math.Pow(mas[1],2)) ;
     }
-    public static double[] gradientSearch (double mas[]){
-        double[] masResult=new double[mas.length];
+    public static double[] gradientSearch (double[] mas){
+        double[] masResult=new double[mas.Length];
         double delta = 0.000000001;
-        double[] masTemp = new double[mas.length];
-        for(int i=0;i<mas.length;i++)
+        double[] masTemp = new double[mas.Length];
+        for(int i=0;i<mas.Length;i++)
             masTemp[i] = mas[i];
-        for(int i=0;i<mas.length;i++){
+        for(int i=0;i<mas.Length;i++){
             masTemp[i]+=delta;
             double elem = (double) (objectiveFunction(masTemp) -objectiveFunction(mas))/delta;
             masResult[i]=elem;
-            for(int j=0;j<mas.length;j++)
+            for(int j=0;j<mas.Length;j++)
                 masTemp[j] = mas[j];
         }
         return masResult;    
@@ -72,9 +73,9 @@ public class TPR_6sem_4Laba {
 }
 
 public class Element {
-    double x1;
-    double x2;
-    double result;
+    public double x1;
+    public double x2;
+    public double result;
     public Element(double x1, double x2,double result) {
         this.x1 = x1;
         this.x2 = x2;
